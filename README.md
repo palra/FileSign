3c695c602d174331f02cf31a1e7f324eefab3f73
Loïc Payol <contact@loicpayol.fr>
2016-12-10 00:14:07 +0100

    Added fr translation

---
+++ README.md
@@ -0,0 +1,14 @@
+FileSign
+========
+
+## Prérequis
+
+* PHP >= 5.5.9
+* PHP OpenSSL extension
+* PHP XML extension
+
+## Installation
+
+* `composer install`
+* `cp app/parameters.php.dist app/parameters.php`
+* Éditez le fichier `app/parameters.php` selon vos besoins
--- app/Application.php
+++ app/Application.php
@@ -8,7 +8,14 @@
  * file that was distributed with this source code.
  */
 
+use Palra\OpenSSL\PrivateKey;
+use Palra\OpenSSL\PublicKey;
 use Silex\Application as BaseApplication;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Translator;
 
 class Application extends BaseApplication
 {
@@ -20,12 +27,18 @@ class Application extends BaseApplication
         $values = array_merge($values, require __DIR__ . '/parameters.php');
         parent::__construct($values);
 
-        $this->register(new \Silex\Provider\TwigServiceProvider(), array(
+        $this->register(new TwigServiceProvider(), array(
             'twig.path' => __DIR__ . '/../src/App/Resources/views'
+        ));
+
+        $this->register(new LocaleServiceProvider());
+        $this->register(new TranslationServiceProvider(), array(
+            'locale_fallbacks' => array('fr')
         ));
 
         $this->registerOpenSSL();
         $this->registerControllers();
+        $this->configureTranslator();
     }
 
     private function registerOpenSSL()
@@ -33,11 +46,11 @@ class Application extends BaseApplication
         $app = $this;
 
         $this['public_key'] = function ($app) {
-            return \Palra\OpenSSL\PublicKey::fromFilePath($app['public_key.path']);
+            return PublicKey::fromFilePath($app['public_key.path']);
         };
 
         $this['private_key'] = $this->protect(function ($passphrase = "") use ($app) {
-            return \Palra\OpenSSL\PrivateKey::fromFilePath($app['private_key.path'], $passphrase);
+            return PrivateKey::fromFilePath($app['private_key.path'], $passphrase);
         });
     }
 
@@ -53,5 +66,20 @@ class Application extends BaseApplication
             ->bind('fs_sign_post');
         $this->get('/public_key', 'App\\Controller\\FileSignController::publicKeyDownloadAction')
             ->bind('fs_public_key');
+    }
+
+    private function configureTranslator()
+    {
+        $this->extend('translator', function (Translator $translator) {
+            $translator->addLoader('yaml', new YamlFileLoader());
+
+            $list = glob(__DIR__ . '/../src/App/Resources/translations/*.yml');
+            foreach ($list as $file) {
+                $file = new SplFileInfo(realpath($file));
+                $translator->addResource('yaml', $file->getPathname(), $file->getBasename('.' . $file->getExtension()));
+            }
+
+            return $translator;
+        });
     }
 }
